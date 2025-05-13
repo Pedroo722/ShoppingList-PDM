f8ec65398c50c53330fbba6eba10cad3834b58f8
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2025-05-13 16:03:18 -0300

    Commit Inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { Provider as PaperProvider } from 'react-native-paper';
 
-export default function App() {
+import Pantry from './src/screens/Pantry';
+import ShoppingList from './src/screens/ShoppingList';
+import Purchase from './src/screens/Purchase';
+
+const Tab = createBottomTabNavigator();
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName: React.ComponentProps<typeof Ionicons>['name'];
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+              if (route.name === 'Pantry') {
+                iconName = focused ? 'home' : 'home-outline';
+              } else if (route.name === 'Shopping List') {
+                iconName = focused ? 'cart' : 'cart-outline';
+              } else {
+                iconName = focused ? 'list' : 'list-outline';
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
+          <Tab.Screen name="Dispensa" component={Pantry} />
+          <Tab.Screen name="Lista de Compras" component={ShoppingList} />
+          <Tab.Screen name="Compras" component={Purchase} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
+
+export default App;
